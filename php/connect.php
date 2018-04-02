c86331a054401e676c79aa47716184eceebb3392
Wishvanath Sah <anandwishvanath@gmail.com>
2018-04-02 16:04:48 +0530

    Add files via upload

---
+++ php/connect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "dpt_cs";
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
